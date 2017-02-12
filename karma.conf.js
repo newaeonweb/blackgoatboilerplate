4ad17e17929fced3345a604d06a17109c0ce4e60
newaeonweb <fernando@newaeonweb.com.br>
2017-02-12 15:31:51 -0200

    rebuild all the shit

---
+++ karma.conf.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const webpackConfig = require('./webpack.config');
+const entry = './app/modules/**/**/__tests__/*.js';
+const preprocessors = {};
+preprocessors[entry] = ['webpack'];
+
+// Karma configuration
+module.exports = function (config) {
+    config.set({
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['mocha', 'chai'],
+
+
+        // list of files / patterns to load in the browser
+        files: [entry],
+
+        webpack: webpackConfig,
+
+        // list of files to exclude
+        exclude: [],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: preprocessors,
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
+
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['Safari'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity,
+
+        plugins: [
+            require('karma-webpack'),
+            'karma-chai',
+            'karma-mocha',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-safari-launcher'
+
+        ]
+    })
+};
